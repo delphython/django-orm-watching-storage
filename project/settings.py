dfc1ed35d58d2968ea60bda21718bcda37f3948f
delphython <delphython@gmail.com>
2021-08-17 06:55:40 +0300

    Add prefix DB_ for database connections env

--- project/settings.py
+++ project/settings.py
@@ -1,18 +1,22 @@
 import os
 from dotenv import load_dotenv
+import dj_database_url
+
 
 load_dotenv()
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ['ENGINE'],
-        'HOST': os.environ['HOST'],
-        'PORT': os.environ['PORT'],
-        'NAME': os.environ['NAME'],
-        'USER': os.environ['USER'],
-        'PASSWORD': os.environ['PASSWORD'],
+        'DB_ENGINE': os.environ['DB_ENGINE'],
+        'DB_HOST': os.environ['DB_HOST'],
+        'DB_PORT': os.environ['DB_PORT'],
+        'DB_NAME': os.environ['DB_NAME'],
+        'DB_USER': os.environ['DB_USER'],
+        'DB_PASSWORD': os.environ['DB_PASSWORD'],
     }
 }
+
+DATABASES['default'] = dj_database_url.parse('postgres://...', conn_max_age=600)
 
 INSTALLED_APPS = ['datacenter']
 
